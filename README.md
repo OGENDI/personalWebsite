1349f6e26d3d4e9499b9b8a48e35f86ffc982d4c
DERICK OGENDI <demokonge17@gmail.com>
2020-11-15 00:17:38 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# personalWebsite
-A website that displays personal information.
+#test2
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Derick Personal website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <img src="images/Derick%20at%20UNODC%20function.png">
+        <nav>
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="Home">
+            <div class="section-inner">
+                <img src="images/Derick%20at%20UNODC%20function.png">
+                <h1>Hi! I'm Derick, upcoming Fullstack developer</h1>
+            </div>
+
+        </section>
+        <section id="About">
+            <div class="section-inner">
+                <h2>About me</h2>
+                <p>I'm a really open minded programmer</p>
+                <h3>Achievements</h3>
+                <ul>
+                    <li>Completed Basics of Digital Literacy offered by JA</li>
+                    <li>I coded this website from scratch!</li>
+                </ul>
+            </div>
+
+        </section>
+        <section id="Contact">
+            <div class="section-inner">
+                <h2>Contact me</h2>
+                <p>You can find me on:</p>
+                <ul>
+                    <li><a href="https://twitter.com/dogendi17">Twitter</a></li>
+                    <li><a href="https://github.com/OGENDI">GitHub</a></li>
+                    <li><a href="https://app.slack.com/client/TK4L8H3SQ/D01CQQHUYN9/user_profile/U01CD5CG04X">Slack</a></li>
+                </ul>
+                <p>Or, you can <a href="mailto:ogendi18@gmail.com">send me an email</a>.</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        Copyright Derick, upcoming Fullstack developer, @2020
+    </footer>
+</body>
+
+</html>
